0b48dc2a12e7839c742422db335a985c90931b5e
Alcopar01 <85458874+Alcopar01@users.noreply.github.com>
2022-05-16 12:40:38 -0500

    Add files via upload

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+ 
+ public function welcome(){
+  
+    return view('welcome');
+ }
+  public function venta(){
+  
+    return view('venta');
+ }
 }
